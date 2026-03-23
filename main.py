bb191239808c056226afeb12f118e4fae5b60125
alanyumbo123 <aj.yumbog@uea.edu.ec>
2026-03-22 21:56:45 -0500

    Archivo principal

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import tkinter as tk
+from servicios.visita_servicio import VisitaServicio
+from ui.app_tkinter import AppVisitas
 
+
+def main():
+
+    root = tk.Tk()
+
+    servicio = VisitaServicio()
+
+    # INYECCIÓN DE DEPENDENCIA
+    app = AppVisitas(root, servicio)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
