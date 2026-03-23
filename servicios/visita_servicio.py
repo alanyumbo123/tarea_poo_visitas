deca325cbd8970a7f7d71081ca7dbfa057aa16b7
alanyumbo123 <aj.yumbog@uea.edu.ec>
2026-03-22 21:54:57 -0500

    Crear servicio CRUD de visitas

--- servicios/visita_servicio.py
+++ servicios/visita_servicio.py
@@ -1,1 +1,36 @@
+# Lógica del sistema (CRUD)
 
+from modelos.visitante import Visitante
+
+
+class VisitaServicio:
+
+    def __init__(self):
+        # lista privada de visitantes
+        self._visitantes = []
+
+    # CREATE
+    def registrar_visitante(self, cedula, nombre, motivo):
+
+        # validar duplicados
+        for v in self._visitantes:
+            if v.cedula == cedula:
+                return False
+
+        visitante = Visitante(cedula, nombre, motivo)
+        self._visitantes.append(visitante)
+        return True
+
+    # READ
+    def obtener_visitantes(self):
+        return self._visitantes
+
+    # DELETE
+    def eliminar_visitante(self, cedula):
+
+        for v in self._visitantes:
+            if v.cedula == cedula:
+                self._visitantes.remove(v)
+                return True
+
+        return False
